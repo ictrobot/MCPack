f7c1a076bc0dc4f01784d2223a8f50b38a95a985
ictrobot <ictrobot@outlook.com>
2020-03-20 11:01:54 +0000

    Bump version to 2.2

--- core/src/ethanjones/mcpack/util/Version.java
+++ core/src/ethanjones/mcpack/util/Version.java
@@ -3,5 +3,5 @@ package ethanjones.mcpack.util;
 public class Version {
 
   public static final int versionCode = 2;
-  public static final String versionString = "2.1";
+  public static final String versionString = "2.2";
 }
